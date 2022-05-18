6a92190cc46e96e5a2a36dd0a53519011acce796
Travis Dvorak <traviswdvorak@gmail.com>
2022-05-18 13:47:26 -0500

    Assignment2

--- Assignment1_1/src/Assignment1_1.java
+++ Assignment1_1/src/Assignment1_1.java
@@ -1,5 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        System.out.println("Travis Dvorak");
+        System.out.println("Java Web");
+        System.out.println("Logan, IA");
+        System.out.println("Dirt cake");
     }
 }
